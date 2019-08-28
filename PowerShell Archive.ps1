fb0e37b5f3313c37d417fee9dc0e1a9ef77b021e
kjp148 <keananpaul@yahoo.com>
2019-08-27 18:43:03 -0700

    Initial script upload

---
+++ PowerShell Archive.ps1
@@ -0,0 +1,6 @@
+﻿Add-Type -AssemblyName "system.io.compression.filesystem"
+Get-ChildItem -Directory | ForEach-Object {
+    $source = $_.FullName
+    $destination = $_.FullName + ".zip"
+    [io.compression.zipfile]::CreateFromDirectory($source, $destination)
+}
